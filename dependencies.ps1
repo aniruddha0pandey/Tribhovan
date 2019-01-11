c75e07c6041a4e4295e47466e63d7aa471abd90a
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-11 17:26:07 +0530

    Update dependencies.ps1

--- dependencies.ps1
+++ dependencies.ps1
@@ -1,9 +1,12 @@
-$source = 'https://www.7-zip.org/a/7z1806-x64.exe'
-$currentDir = (Get-Location).tostring()
-curl $source -Outfile $currentDir + '\7z1806-x64.exe'
 $source = @()
-$source = 'https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-win64.zip'
-curl $source -Outfile $currentDir + '\geckodriver-v0.23.0-win64.zip'
+$source += 'https://www.7-zip.org/a/7z1806-x64.exe'
+$source += 'https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-win64.zip'
+
+ForEach ( $item in $source ) {
+	$file = ($item).split('/')[-1]
+	curl $item -Outfile $currentDir + '\' + $file
+}
+
 start 7z1806-x64.exe
 C:\Program` Files\7-Zip\7z x geckodriver-v0.23.0-win64.zip
 echo "$env:Path += ';' + $currentDir" >> %UserProfile%\Documents\WindowsPowerShell\profile.ps1
