07dfe47a6208e49435e94cec63162a72ee186300
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-11 14:55:05 +0530

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -3,3 +3,8 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("https://quantumexperience.ng.bluemix.net/qx/account/advanced")
 assert "IBM Q" in driver.title
+
+
+
+
+driver.close()
