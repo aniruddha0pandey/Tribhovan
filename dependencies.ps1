f73c503f20e626814049dd72edaafa12d8ea4470
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-11 17:30:41 +0530

    Update dependencies.ps1

--- dependencies.ps1
+++ dependencies.ps1
@@ -3,8 +3,8 @@ $source += 'https://www.7-zip.org/a/7z1806-x64.exe'
 $source += 'https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-win64.zip'
 
 ForEach ( $item in $source ) {
-	$file = ($item).split('/')[-1]
-	curl $item -Outfile $currentDir + '\' + $file
+	$file = $currentDir + '\' + ($item).split('/')[-1]
+	curl $item -Outfile $file
 }
 
 start 7z1806-x64.exe
