004600c201bfbee23545f3755b2f71b8baeedd36
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-11 15:36:43 +0530

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -3,10 +3,12 @@ try:
   assert "" not in API_TOKEN
 except:
   from selenium import webdriver
+  import clipboard
 
   driver = webdriver.Firefox()
   driver.get("https://quantumexperience.ng.bluemix.net/qx/account/advanced")
   assert "IBM Q" in driver.title
   copy_api_token = driver.find_element_by_id('button')
   copy_api_token.click()
+  API_TOKEN = clipboard.paste()
   driver.close()
