507f3bd1b7765905726bad3a17c519ca0d10ae0b
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-11 16:32:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from get_token import *
 from qiskit import IBMQ
 
+
+# Authenticate for access to remote backends
+try:
+    IBMQ.enable_account(API_TOKEN)
+except:
+    print("""WARNING: There's no connection with the API for remote backends.
+             Have you initialized a file with your personal token?
+             For now, there's only access to local simulator backends...""")
+
+
 def main():
-  IBMQ.enable_account(API_TOKEN)
+  pass
+  
 
 if __name__ == "__main__":
   main()
