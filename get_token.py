e2aea801b6fd92fd0717a05463563cacfad22440
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-01-11 15:18:22 +0530

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -7,6 +7,6 @@ except:
   driver = webdriver.Firefox()
   driver.get("https://quantumexperience.ng.bluemix.net/qx/account/advanced")
   assert "IBM Q" in driver.title
-  button = driver.find_element_by_id('button')
-  button.click()
+  copy_api_token = driver.find_element_by_id('button')
+  copy_api_token.click()
   driver.close()
