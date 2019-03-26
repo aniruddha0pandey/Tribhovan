fc4f4586fb20a55f49955b9daa080448410b9521
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-03-26 21:18:52 +0530

    Update dependencies.ps1

--- dependencies.ps1
+++ dependencies.ps1
@@ -9,4 +9,4 @@ ForEach ( $item in $source ) {
 
 start 7z1806-x64.exe
 C:\Program` Files\7-Zip\7z x geckodriver-v0.23.0-win64.zip
-echo "$env:Path += ';' + $currentDir" >> %UserProfile%\Documents\WindowsPowerShell\profile.ps1
+echo "$env:Path += ';' + $currentDir" >> %UserProfile%\Documents\WindowsPowerShell\profile.ps1 # Microsoft.PowerShell_profile.ps1
